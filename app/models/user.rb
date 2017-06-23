8389b225c3ff0226975685d67684e44c5cece7f1
jenniferwiner <jeniwiner@gmail.com>
2017-06-22 20:22:25 -0600

    bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   has_many :scores
   has_many :alternatives
 
+  #bcrypt
+  has_secure_password
+
   validates :username, presence: true, length: { minimum: 5 }, uniqueness: true
   validates :first_name, :last_name, :password_digest, :patient_number, :phone_number, presence: true
 end
