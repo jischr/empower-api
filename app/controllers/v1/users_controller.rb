7d9774a0254d09dd0f08f5f087b3f78e8360aaa9
Emily Ringoen <emilyaringoen@gmail.com>
2017-06-26 17:29:29 -0600

    fetch to users with alternatives attached

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -3,7 +3,8 @@ class V1::UsersController < ApplicationController
   skip_before_action :authenticate_request
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]).as_json(include: {alternatives: {only: [:id, :text]}}, except: :password_digest)
+    puts @user
     render json: @user
   end
 
