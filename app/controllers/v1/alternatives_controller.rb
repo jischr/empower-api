acdfc9fe0b25435cf1b8aaa9662e3479f701dd27
jenniferwiner <jeniwiner@gmail.com>
2017-06-26 17:02:20 -0600

    CRUD for alternatives

--- app/controllers/v1/alternatives_controller.rb
+++ app/controllers/v1/alternatives_controller.rb
@@ -1,8 +1,37 @@
 class V1::AlternativesController < ApplicationController
   skip_before_action :authenticate_request
 
-  def index
-    @alternatives = Alternative.all
+  def show
+    @alternatives = User.find(params[:id]).alternatives.as_json(only: :text)
     render json: @alternatives
+  end
+
+  def create
+    @alternative = Alternative.new(alternative_params)
+    if @alternative.save
+      render json: @alternative
+    else
+      render :status => 400
+    end
+  end
+
+  def update
+    @alternative = Alternative.find(params[:id])
+    if @alternative.update(alternative_params)
+      render json: @alternative
+    else
+      render :status => 400
+    end
+  end
+
+  def destroy
+     @alternative = Alternative.find params[:id]
+     @alternative.destroy
+  end
+
+  private
+
+  def alternative_params
+    params.require(:alternative).permit(:text, :user_id)
   end
 end
--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,2 +1,10 @@
 class V1::UsersController < ApplicationController
+
+  skip_before_action :authenticate_request
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
 end
